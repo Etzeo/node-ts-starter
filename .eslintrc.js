62d488ddb54f54da394ae5b8dcb3e8344e48d315
Joshua Tsang <14105275+Etzeo@users.noreply.github.com>
2021-07-23 21:58:41 +0800

    chore: add Prettier to ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:unicorn/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     project: './tsconfig.json',
