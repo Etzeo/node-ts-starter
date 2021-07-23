001d2fba5496480038ddf8f689ba9ce32204a791
Joshua Tsang <14105275+Etzeo@users.noreply.github.com>
2021-07-23 21:45:19 +0800

    build(deps-dev): add ESLint deps, scripts and config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:unicorn/recommended',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'unicorn/custom-error-definition': 'error',
+    'unicorn/no-unsafe-regex': 'error',
+    'unicorn/filename-case': ['error', { case: 'camelCase' }],
+    'unicorn/no-useless-undefined': ['error', { checkArguments: false }],
+    'unicorn/no-array-reduce': 'off',
+  },
+};
